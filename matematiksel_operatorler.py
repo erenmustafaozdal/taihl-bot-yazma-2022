80d455ecc4373b5652b38b81b7d3579355d7ebfb
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-10-08 11:56:58 +0300

    Matematiksel operatörler öğrenildi

--- matematiksel_operatorler.py
+++ matematiksel_operatorler.py
@@ -7,3 +7,26 @@ Bölme: /
 Mod: %
 Tam Bölme: //
 """
+
+# TOPLAMA
+print(5487+9241)
+
+# ÇIKARMA
+print(42-17)
+
+# ÇARPMA
+print(12*16)
+
+# BÖLME
+print(81/4)
+
+# ÜS ALMA
+print(2**3)
+print(2**3**4)
+
+# MOD ALMA
+print(40%3)
+
+# TAM BÖLME
+print(40//3)  # 13
+print(-40//3)  # -14

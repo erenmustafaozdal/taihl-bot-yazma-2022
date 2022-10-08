a1470fe19590fda989f367cfc3e1c57c6ab534f7
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-10-08 11:26:48 +0300

    Sayı veri tipleri öğrenildi

--- sayi_veri_tipleri.py
+++ sayi_veri_tipleri.py
@@ -3,3 +3,13 @@
     int: 1, 75
     float: 8.5, 12.8
 """
+
+# TAM SAYILARLA İŞLEM YAPALIM
+print(8+7)
+
+# ONDALIK SAYILARLA İŞLEM YAPALIM
+print(1.6*3.7)
+
+# VERİLERİN TİPİNE BAKALIM: type()
+print(type(5))
+print(type(19.7))

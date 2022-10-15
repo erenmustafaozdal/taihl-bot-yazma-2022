2eb165869e35a97aed594ff77f5f1bb408383a6b
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-10-15 12:46:05 +0300

    3. hafta ödevinin tamamı

--- odev_3hafta.py
+++ odev_3hafta.py
@@ -1,4 +1,18 @@
 url = "https://teknolojiaihl.meb.k12.tr"
 
-# 1- "url" içinde kaç karakter olduğunu yazdır
+# 1- "url" içinde kaç karakter olduğunu yazdır.
 print(len(url))
+
+# 2- "url" içindeki "meb" sözcüğünü ekrana yazdırın.
+
+# 3- "url" içindeki "k12" sözcüğünü ekrana yazdırın.
+
+# 4- Kullanıcıdan adını, en sevdiği yemeği alın ve cümle içinde yazdırın.
+# ÖRNEK CÜMLE: Adınız: Eren. En sevdiğiniz yemek: Pırasa
+
+# 5- Öğrencinin 2 sınav notunu alın. Notunu hesaplayıp cümle içinde yazdırın.
+# 1. sınav oranı: %35
+# 2. sınav oranı: %65
+# ÖRNEK CÜMLE: 1. sınav: 70 puan, 2. sınav: 90 puan, Notunuz: 83.0
+
+# 6- Kullanıcının adını alın ve yan yana 100 defa yazdırın.

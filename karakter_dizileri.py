aa6dfa5d2b19c37df5053d0de783e3f5e6153852
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-10-15 11:30:17 +0300

    Karakter dizileri bitti

---
+++ karakter_dizileri.py
@@ -0,0 +1,17 @@
+# Kullanıcı yaş ve isim değişkenlerini oluşturalım
+yas = 36
+isim = "Eren Mustafa Özdal"
+
+# Kullanıcı bilgilerini ekrana yazdıralım
+# -> Adınız: Eren, Yaşınız: 36
+print("Adınız: " + isim + ", Yaşınız: " + str(yas))
+
+# karakter verisinin uzunluğuna bakalım
+print(len(isim))
+
+# str veri tipindeki değerin çeşitli elemanlarını yazdıralım
+# elemanlar 0'dan başlar
+print(isim[0])
+print(isim[1])
+print(isim[2])
+print(isim[3])

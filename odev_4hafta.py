9ccf89657f2b9ba66f97ecfb89ac6dc3fef24d76
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-10-22 13:35:23 +0300

    4. hafta ödevi

---
+++ odev_4hafta.py
@@ -0,0 +1,28 @@
+# Aşağıdaki adreste yer alan haber metnini "haber" değişkenine atadık
+# Adres: https://teknolojiaihl.meb.k12.tr/icerikler/20222023-egitim-ogretim-yili-acilis-dersimiz_13176230.html
+# Bir başka string tanımlama yöntemi olarak
+# 3 adet tırnak (""") veya 3 adet kesme işareti (''') kullanılabilir.
+# Bu yöntem ile birden fazla satırdan oluşan stringler tanımlanabilir.
+haber = """2022/2023 Eğitim-Öğretim Yılı Açılış Dersimiz.
+Okulumuz Sancaktepe Teknoloji Anadolu İmam Hatip Lisesinin 2022/2023 Eğitim-Öğretim Yılı açılış dersi Prof. Dr. Bahadır TUNABOYLU tarafından verildi. Okul Danışma Kurulu Başkanımız Sayın Ahmet AKÇA'nın da öğrencilerimize selamlama konuşması yaptığı açılış dersimizde Prof. Dr. Bahadır TUNABOYLU hocamız uzun yıllara dayanan hayat, iş ve kariyer tecrübelerini öğrencilerimizle paylaştılar.
+Kaliforniya Üniversitesi elektronik ve bilgisayar mühendisliği- malzeme bilimi programında doktora yaptıktan sonra silikon vadisinde ve Arizona'da 20 yıla yakın süreyle mikroçip ve nanoteknoloji, test sistemleri, mikroelektromekanik sistemler, nanomalzemeler, yeni üretim teknolojileri alanında ve ürün geliştirme programlarında çalışan Prof. Dr. Bahadır TUNABOYLU, 2016-2020 yılları arasında Tübitak MAM, BİLGEM, Bilişim Vadisi ve Sabancı Üniversitesi SUNUM yönetim kurulu üyelikleri görevlerinde bulundu. BM ilişkili Uluslararası Endüstriyel Araştırma Organizasyonları yönetim kurulu üyeliği, Ortadoğu ve Kuzey Afrika bölge temsilcisi görevlerinde bulundu. Halen Çevre Vakfı Yönetim Kurulu, MEB ARGEMER Bilimsel Danışma Kurulu ve okulumuz Sancaktepe Teknoloji Anadolu İmam Hatip Lisesi Danışma Kurulu üyesi olup Marmara Üniversitesi'nde Inovasyon ve Teknoloji Transfer programlarını yönetmektedir."""
+
+# 1- Haber kaç paragraftan oluşuyor?
+# ❗ Bu hafta yetişmedi. Stringler gibi listelerin eleman sayısı da len() ile bulunur
+
+# 2- Haberin ilk paragrafının tüm harflerini büyük yapıp
+
+# 3- Haberin 2. paragrafını her kelimenin ilk harfini büyük yapıp
+
+# 4- Haberdeki "2022/2023" ifadelerini "2023/2024" olarak değiştirelim: replace()
+
+# 5- Haberi küçük harfe çevirip, "teknoloji" kelimesi kaç defa geçiyor bakalım
+
+# 6- Haberin içindeki yeni satır ifadelerini silin.
+# Ardından noktalardan bölüp, her elemanı ayrı satıra yazdırın
+
+# 7- Öğrenci isim ve yaş bilgilerinden oluşan listeleri birleştirelim
+ogrenci_1 = ["Ahmet", 12]
+ogrenci_2 = ["Ayşe", 16]
+
+# 8- Birleştirilmiş listeden ikinci öğrencinin yaşını ekrana yazdıralım

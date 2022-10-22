620b9ce71f7599d30e90113fe79e01789844edab
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-10-22 11:04:56 +0300

    String kaçış ifadeleri önsöz

---
+++ string_kacis_dizileri.py
@@ -0,0 +1,6 @@
+"""
+Python’ın gözünde bir başlangıç tırnağı ile bitiş tırnağı arasında yer alan
+her şey bir karakter dizisidir. Ancak bizler metin içerisinde bu tırnakları
+özel bir amaçla kullanmak isteyebiliriz. Bu tür durumlarda kaçış karakteri
+olan ters bölü işaretini (\) kullanarak hatadan kaçarız.
+"""

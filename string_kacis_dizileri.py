c0b43bf98066b9da0c79cf29c473885ba79f6a6e
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-10-22 11:22:18 +0300

    String kaçış dizeleri bitti

--- string_kacis_dizileri.py
+++ string_kacis_dizileri.py
@@ -4,3 +4,14 @@ her şey bir karakter dizisidir. Ancak bizler metin içerisinde bu tırnakları
 özel bir amaçla kullanmak isteyebiliriz. Bu tür durumlarda kaçış karakteri
 olan ters bölü işaretini (\) kullanarak hatadan kaçarız.
 """
+
+print("Ahmet, \"Bugün sinemaya gidiyorum.\" dedi.")
+
+print('Yarın Adana\'ya gidiyorum.\n')
+
+print("C:\\Users\\erenMustafaOzdal")
+
+# ÖZEL KARAKTERLER
+print("Eren\nMustafa\nÖzdal")  # alt satır
+print("Eren\tMustafa\tÖzdal")  # tab
+print("Ahmet\tMehmet\tYılmaz")  # tab

335693b56a66091f1ef9e6294387725607793655
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-11-26 13:54:01 +0300

    6. hafta ödevi

---
+++ odev_6hafta.py
@@ -0,0 +1,54 @@
+# 1- "Türkçe, Hayat Bilgisi, Matematik, Fen Bilimleri" elemanlarından oluşan liste oluşturalım
+
+# 2- "Hayat Bilgisi" değerini "Sosyal Bilgiler" değeri ile değiştirelim
+
+# 3- "Coğrafya" listenin bir elemanı mıdır?
+
+# 4- Listenin son 2 elemanı yerine "Görsel Sanatlar" ve "Müzik" değerlerini ekleyelim
+
+# 5- Listenin üzerine "Serbest Etkinlik" ve "Beden Eğitimi ve Oyun" değerlerini ekleyelim
+
+# 6- Listenin son elemanını silelim
+
+# 7- Liste elemanlarını ters çevirelim
+
+# 8- Liste elemanlarını alfabetik olarak sıralayalım
+
+# 9- 3 öğrenci için öğrenci no anahtarı ile ad, soyad, cinsiyet, yaş bilgilerinden oluşan bir dictionary oluşturalım
+}
+
+# 10- Kullanıcıdan öğrenci numarası alarak öğrenci bilgilerini ekrana yazdıralım
+# ÖRNEK METİN: 100 nolu öğrencinin adı Eren Özdal ve cinsiyeti erkek
+
+# 11- Kullanıcıdan 2 sayı alalım ve bu sayıları "a" ve "b" değişkenlerine atayalım.
+
+# 12- "a"nın "b" ile toplamını "a" değişkenine atayıp ekrana yazdıralım.
+
+# 13- "a"nın "b" ile farkını "a" değişkenine atayıp ekrana yazdıralım.
+
+# 14- "a"nın "b" ile çarpımını "a" değişkenine atayıp ekrana yazdıralım.
+
+# 15- "a"nın "b"ye bölümünü "a" değişkenine atayıp ekrana yazdıralım.
+
+# 16- "a"nın "b"ye bölümünden kalanı "a" değişkenine atayıp ekrana yazdıralım.
+
+# 17- "a"nın "b" üssünü "a" değişkenine atayıp ekrana yazdıralım.
+
+# 18- "a"nın "b"ye kalansız bölümünü "a" değişkenine atayıp ekrana yazdıralım.
+
+# 19- Kullanıcıdan 2 sayı alıp, 1. sayı 2. sayıdan büyük mü ekrana yazdıralım.
+# ÖRNEK METİN: 1. sayı, 2. sayıdan büyük mü: True
+
+# 20- Kullanıcıdan 1. sınav (%40) ve 2. sınav notu (%60) alıp ortalama hesaplayalım.
+# Eğer ortalama 45 üzerindeyse "True", değilse "False" yazdıralım.
+# ÖRNEK METİN: 1. sınav: 50, 2. sınav: 70, Ortalama: 62. Geçti mi: True
+
+# 21- Kullanıcıdan bir sayı alalım ve bu sayı tekse "True", değilse "False" yazdıralım.
+# ÖRNEK METİN: Girilen sayı: 5. Tek mi: True
+
+# 22- Kullanıcıdan bir sayı alalım ve bu sayı pozitifse "True", değilse "False" yazdıralım.
+# ÖRNEK METİN: Girilen sayı: -10. Pozitif mi: False
+
+# 23- Kullanıcıdan alınan bir sayının 0-100 arasında olup olmadığını
+# "and" operatörü ile bulup ekrana yazdıralım
+# ÖRNEK METİN: Sayı: 110. 0-100 arasında mı: False

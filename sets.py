aa8be4eb3fb08a881c1887bfd163484fb78d9634
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-11-26 09:34:00 +0300

    sets ön bilgi

---
+++ sets.py
@@ -0,0 +1,7 @@
+"""
+sets listeleri süslü parantezler '{}' içinde tanımlanır.
+sets liste elemanlarına indeks numaraları ile ulaşılamaz.
+sets liste elemanlarına döngü içinde ulaşılır.
+sets listeleri içinde aynı eleman birden fazla yer alamaz.
+"""
+

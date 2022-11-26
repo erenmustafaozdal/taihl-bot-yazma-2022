03a849aae2261feb9bc7de88ed241e9e92792ad7
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-11-26 10:06:33 +0300

    value ve referans veri tipleri ön bilgi

---
+++ referans.py
@@ -0,0 +1,17 @@
+"""
+VALUE VERİ TİPİ (VALUE TYPES)
+//////////////////////////////////////////////////////////////////////////////
+✔ Verileri kendine ayrılmış bir bellekte tutar.
+✔ Değişken doğrudan bir değeri tutar.
+✔ Başka bir değişkene atarsanız, değer doğrudan kopyalanır.
+    ⚫ İki değişken de bağımsız çalışır.
+ÖR: string, int, float
+
+REFERANS VERİ TİPİ (REFERENCE TYPES)
+//////////////////////////////////////////////////////////////////////////////
+✔ Gerçek verilerin bellekteki konumunu tutar.
+✔ Verinin kendisini değil, adresini temsil eder.
+✔ Başka bir değişkene atarsanız, veriler kopyalanmaz.
+    ⚫ Orijinal verinin adresini başka değişkene atamış olursunuz.
+ÖR: list, dict
+"""

1599cda793a2fed0f4e5119a0e4729f30f61ff21
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-11-26 10:48:38 +0300

    referans veri tipi

--- referans.py
+++ referans.py
@@ -15,3 +15,30 @@ REFERANS VERİ TİPİ (REFERENCE TYPES)
     ⚫ Orijinal verinin adresini başka değişkene atamış olursunuz.
 ÖR: list, dict
 """
+
+# VALUE TİPİ
+###########################################
+# iki value veri tipi oluşturalım
+a = 1
+b = 2
+
+# ikinci değişkeni ilk değişkene atayalım
+a = b
+
+# ikinci değişkenin değerini değiştirelim. birinci değişken değişti mi bakalım.
+b = 3
+print(a, b)
+
+
+# REFERANS TİPİ
+###########################################
+# iki tane referan tipi değişken oluşturalım
+x = [1, 2, 3]
+y = ["a", "b" ,"c"]
+
+# ikinci değişkeni ilk değişkene atayalım
+x = y
+
+# ikinci değişken değişiklik yaptığımızda ilk değişken etkileniyor mu bakalım.
+y[0] = "d"
+print(x, y)

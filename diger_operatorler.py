16743c5a72afb1c00919afeb02eb649e75020514
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-11-26 12:01:00 +0300

    Diğer operatörler dersi

--- diger_operatorler.py
+++ diger_operatorler.py
@@ -9,3 +9,17 @@ DİĞER OPERATÖRLER
 | in       | üyelik operatörü | "a" in x  |
 +----------+------------------+-----------+
 """
+
+# tek liste ile 2 farklı değişken ve ayrı bir liste ile 3. değişken oluşturalım
+a = b = [1, 2, 3]
+c = [1, 2, 3]
+
+# değişkenlerin değer eşitliğini (==) ve kimlik eşitliğini (is) kontrol edelim
+print(a == b)
+print(a == c)
+print(a is b)
+print(a is c)
+
+# bir eleman listenin içindeki bir eleman mıdır kontrol edelim
+print(3 in a)
+print(4 in a)

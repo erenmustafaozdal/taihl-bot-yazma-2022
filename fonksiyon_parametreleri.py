2de53e153f349eac3fa3e001c53ca62fd207d2a7
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-12-24 09:30:19 +0300

    Fonksiyon parametrelerinde varsayılan parametre

--- fonksiyon_parametreleri.py
+++ fonksiyon_parametreleri.py
@@ -13,6 +13,21 @@ def ogrenci_kaydet(ad, soyad, notlar):
     dosya.close()
 
 
+def ogrenci_kaydet2(ad, soyad, notlar=None):
+    if notlar:  # if isinstance(notlar, str):
+        # notları integer yap ve ortalamayı hesapla
+        notlar_list = [int(x) for x in notlar.split(",")]
+        ortalama = sum(notlar_list) / len(notlar_list)
+    else:
+        notlar = "Not bilgisi yok"
+        ortalama = "Ortalama hesaplanmadı"
+
+    # dosyaya yaz
+    dosya = open("öğrenciler.txt", mode="a", encoding="utf-8")
+    dosya.write(f"Ad: {ad}, Soyad: {soyad}, Notlar: {notlar}, Ortalama: {ortalama}\n")
+    dosya.close()
+
+
 while True:
     ne_yapilacak = input("Ne yapacaksınız? (1: Kaydet, 0: Çık): ")
 
@@ -22,4 +37,7 @@ while True:
     ad = input("Öğrenci Adı: ")
     soyad = input("Öğrenci Soyadı: ")
     notlar = input("Sınav notları (virgülle ayrılmış): ")
-    ogrenci_kaydet(ad, soyad, notlar)
+    if notlar != "":
+        ogrenci_kaydet2(ad, soyad, notlar)
+    else:
+        ogrenci_kaydet2(ad, soyad)

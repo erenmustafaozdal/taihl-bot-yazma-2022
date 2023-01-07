49e4d5f2ed1d2d00ec897a83f43e27ca9331cb8f
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2023-01-07 09:15:48 +0300

    Bim aktüel ürünleri işlemerinin algoritması

---
+++ bim.py
@@ -0,0 +1,10 @@
+"""
+1. Aktüel ürünler sayfasına git
+    https://www.bim.com.tr/Categories/100/aktuel-urunler.aspx
+2. Gelecek Hafta'ya tıkla
+3. Tarihlere tıklayarak aşağıdaki işlemleri yap
+    3.1. Ürünler içinde aşağıdaki işlemleri
+        3.1.1. Ürün resmini kaydet
+        3.1.2. Ürün bilgilerini oku
+        3.1.3. Ürün fiyatını oku
+"""
